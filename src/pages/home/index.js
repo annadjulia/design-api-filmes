b432cb6e8436fb7e55d58ccd08d8c9ef07a1addc
annadjulia <annadjulia4815162342@gmail.com>
2023-09-25 01:09:23 -0300

    manequim

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,41 +1,133 @@
 import { useEffect, useState } from "react";
-import { Container, Movie, MovieList, Btn } from "./style";
+import { Mov, Btn, Fav } from "./style";
+import { Container, Row, Col, Image, Navbar } from "react-bootstrap";
+import Slider from "react-slick";
 import { Link } from "react-router-dom";
 
 function Home() {
     const imagePath = "https://image.tmdb.org/t/p/w500";
 
-    const [movies, setMovies] = useState([]);
+    const [list, setList] = useState([]);
+    const [pops, setPops] = useState([]);
+    const [nows, setNows] = useState([]);
+    const [tops, setTops] = useState([]);
+    const [coms, setComs] = useState([]);
     const KEY = process.env.REACT_APP_KEY;
     useEffect(() => {
+        async function carregaList() {
+        fetch(`https://api.themoviedb.org/3/list/8271861?api_key=${KEY}&language=pt-BR`)
+            .then(response => response.json())
+            .then(data => {
+                setList(data.items);
+            });
+        }
+        async function carregaPops() {
         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${KEY}&language=pt-BR`)
             .then((response) => response.json())
             .then((data) => {
-                setMovies(data.results);
+                setPops(data.results);
             });
+        }
+        async function carregaNows() {
+        fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${KEY}&language=pt-BR`)
+            .then((response) => response.json())
+            .then((data) => {
+                setNows(data.results);
+            });
+        }
+        async function carregaTops() {
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${KEY}&language=pt-BR`)
+            .then((response) => response.json())
+            .then((data) => {
+                setTops(data.results);
+            });
+        }
+        async function carregaComs() {
+        fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${KEY}&language=pt-BR`)
+            .then((response) => response.json())
+            .then((data) => {
+                setComs(data.results);
+            });
+        }
+
+        carregaList();
+        carregaPops();
+        carregaNows();
+        carregaTops();
+        carregaComs();
     }, [KEY]);
 
-    return (
-        <Container>
-            <h1>Movies</h1>
-            <MovieList>
-                {movies.map((movie) => {
+    const SliderLegal = (props) => {
+        return (
+            <Slider slidesToScroll={1} slidesToShow={5} dots={true} speed={400}>
+                {props.oloco.map((movie) => {
                     return (
-                        <Movie key={movie.id}>
+                        <Mov key={movie.id}>
                             <img
                                 src={`${imagePath}${movie.poster_path}`}
-                                alt="{movie.title}"
+                                alt={movie.title}
                             />
-                            <span>{movie.title}</span>
+                            <p>{movie.title}</p>
 
                             <Link to={`/${movie.id}`}>
                                 <Btn>Detalhes</Btn>
                             </Link>
-                        </Movie>
+                        </Mov>
                     );
                 })}
-            </MovieList>
-        </Container>
+            </Slider>
+        );
+    }
+
+    document.title = "THE WATCHER";
+
+    return (
+        <div>
+            <Navbar bg="light">
+                <Navbar.Brand>THE WATCHER</Navbar.Brand>
+            </Navbar>
+            <Container>
+                <h1>Favoritos</h1>
+                <Slider slidesToShow={1} slidesToScroll={1} autoplay={true} autoplaySpeed={4000} speed={1100}>
+                    {list.map((movie) => {
+                        return (
+                            <Fav className="container-fluid" key={movie.id}>
+                                <Row>
+                                    <Col md={3}>
+                                        <Image
+                                            src={`${imagePath}${movie.poster_path}`}
+                                            alt={movie.title}
+                                        />
+                                    </Col>
+                                    <Col className="align-content-center" md={9}>
+                                        <Row>
+                                            <p>{movie.title}</p>
+
+                                        <Link to={`/${movie.id}`}>
+                                            <Btn>Detalhes</Btn>
+                                        </Link>
+                                        </Row>
+                                    </Col>
+                                </Row> 
+                            </Fav>
+                        );
+                    })}
+                </Slider>
+            </Container>
+            <Container>
+                <h1>Em cartaz</h1>
+                <SliderLegal oloco={nows} />
+
+                <h1>Populares</h1>
+                <SliderLegal oloco={pops} />
+
+                <h1>Melhores classificações</h1>
+                <SliderLegal oloco={tops} />
+
+                <h1>Em breve</h1>
+                <SliderLegal oloco={coms} />
+            </Container>
+        </div>
     );
 }
 
--- src/pages/movie/index.js
+++ src/pages/movie/index.js
@@ -1,56 +1,122 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { format } from "date-fns";
+import { Navbar, Container, Row, Col, Image, Button, Nav } from 'react-bootstrap';
+import Slider from "react-slick";
 import { Link } from "react-router-dom";
+import { Mov, Btn } from "./style";
 import "./styles.css";
 
 const Movie = () => {
     const { id } = useParams();
     const imagePath = "https://image.tmdb.org/t/p/w500";
-
+    
     const [movie, setMovie] = useState([]);
+    const [alike, setAlike] = useState([]);
+    const [genres, setGenres] = useState([]);
     const KEY = process.env.REACT_APP_KEY;
     useEffect(() => {
-        fetch(
-            `https://api.themoviedb.org/3/movie/popular?api_key=${KEY}&language=pt-BR`
-        )
+        async function carregaFilme() {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${KEY}&language=pt-BR`)
             .then((response) => response.json())
             .then((data) => {
-                const res = data.results;
-                let filme = res.find((key) => {
-                    // eslint-disable-next-line
-                    return key.id == id;
+                let filme = data;
+                data.release_date = format(new Date(data.release_date), "dd/MM/yyyy");
+                data.vote_average = data.vote_average.toFixed(1);
+                let genre = data.genres.map((genre) => {
+                    return genre.id;
                 });
+                setGenres(genre);
+                console.log(genre);
                 setMovie(filme);
+
+                
             }); // eslint-disable-next-line
-    }, []);
+        }
+        async function carregaAlike() {
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&with_genres=${genres.slice(0,3)}&language=pt-BR`)
+                .then((response) => response.json())
+                .then((data) => {
+                    let res = data.results;
+                    res = res.filter((mov) => {
+                        return mov.id !== movie.id;
+                    });
+                    setAlike(res);
+                });
+        }
+        carregaFilme();
+        carregaAlike();
+        window.scrollTo({top: 0, left: 0, behavior: 'auto'});
+    }, [KEY, id, genres]);
+
+    document.title = movie.title;
 
     return (
         <div>
-            <nav>
-                <h1>Movies</h1>
-            </nav>
-            
-            <div className="container">
-                <div>
-                    <img
-                        className="img_movie"
-                        src={`${imagePath}${movie.poster_path}`}
-                        alt="{movie.title}"
-                    />
-                </div>
-                <div>
-                    <h1>{movie.title}</h1>
-                    <h3>Data de lançamento: {movie.release_date}</h3>
-                    <div className="descricao">
-                        <h4>Descrição: </h4>
-                        <p className="movie-desc">{movie.overview}</p>
-                    </div>
-                    <Link to="/">
-                        <button className="link_button">Voltar</button>
-                    </Link>
-                </div>
-                
-            </div>
+            <Navbar bg="light">
+                <Container>
+                    <Image src="/logo-red.png" alt="logo" width="50" height="50" />
+                </Container>
+            </Navbar>
+
+            <Container>
+                <Row>
+                    <Col md={5}>
+                        <Image
+                            src={`${imagePath}${movie.poster_path}`}
+                            alt={movie.title}
+                        />
+                    </Col>
+                    <Col md={7}>
+                        <Row>
+                            <h1>{movie.title}</h1>
+                            <h3>Nota: {movie.vote_average}</h3>
+                            <h3>Data de lançamento: {movie.release_date}</h3>
+                            <div>
+                                <h4>Descrição: </h4>
+                                <p>{movie.overview}</p>
+                            </div>
+                            <div>
+                                <h4>Gêneros: </h4>
+                                <ul>
+                                    {movie.genres &&
+                                        movie.genres.map((genre) => {
+                                            return (
+                                                <li key={genre}>
+                                                    {genre.name}
+                                                </li>
+                                            );
+                                        })}
+                                </ul>
+                            </div>
+                            <Link to="/">
+                                <Button variant="primary">Voltar</Button>
+                            </Link>
+                        </Row>
+                        
+                    </Col>
+                </Row>
+            </Container>
+            <Container>
+                <h1>Filmes parecidos</h1>
+                <Slider slidesToScroll={1} slidesToShow={5} dots={true} speed={400}>
+                    {alike.map((movie) => {
+                        return (
+                            <Mov key={movie.id}>
+                                <img
+                                    src={`${imagePath}${movie.poster_path}`}
+                                    alt={movie.title}
+                                />
+                                <p>{movie.title}</p>
+
+                                <Link to={`/${movie.id}`}>
+                                    <Btn>Detalhes</Btn>
+                                </Link>
+                            </Mov>
+                        );
+                    })}
+                </Slider>
+            </Container>
         </div>
     );
 };
